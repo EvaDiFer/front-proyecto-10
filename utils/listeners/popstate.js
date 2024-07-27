1f68d9d4414ec54621353702521ac2d12e84ece0
EvaDiFer <evadiazfer16@gmail.com>
2024-07-27 12:06:00 +0200

    popstate ready

--- utils/listeners/popstate.js
+++ utils/listeners/popstate.js
@@ -1,17 +1,20 @@
 import { Home } from '../../src/pages/Home/Home';
 import { navLinks } from '../data/navlinks';
 
+const loadPage = (path) => {
+  const link = navLinks.find((link) => link.path === path);
+  if (link) {
+    link.page();
+  } else {
+    Home();
+    window.history.pushState('', '', '/');
+  }
+};
+
 export const popStateListener = () => {
   window.addEventListener('popstate', () => {
-    const link = navLinks.find(
-      (link) => link.path === window.location.pathname
-    );
-    link.page();
-
-    if (!link) {
-      Home();
-
-      window.history.pushState('', '', '/');
-    }
+    loadPage(window.location.pathname);
   });
+
+  loadPage(window.location.pathname);
 };
