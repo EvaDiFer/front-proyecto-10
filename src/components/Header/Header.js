2ee6ca78e22a38cb3e42966d720e0c0dde0c652a
EvaDiFer <evadiazfer16@gmail.com>
2024-07-30 15:28:51 +0200

    finally?

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,23 @@ export const Header = () => {
   const header = document.createElement('header');
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
+  const burgerButton = document.createElement('button');
 
   header.classList.add('header-class');
   nav.classList.add('nav-class');
   ul.classList.add('ul-class');
+  burgerButton.classList.add('burger-button');
+
+  burgerButton.innerHTML = '&#9776;';
 
   app.append(header);
+  header.append(burgerButton);
   header.append(nav);
   nav.append(ul);
+
+  burgerButton.addEventListener('click', () => {
+    ul.classList.toggle('ul-open');
+  });
 
   RenderLinks();
 };
